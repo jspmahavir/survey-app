1a89e818f8edd8d4d06cde6e1318ab7f78cabdf2
Mahavir <jspmahavir@gmail.com>
2020-01-29 12:46:41 +0530

    add actions on survey grid

--- src/pages/userPages/SurveyPage/SurveyPage.tsx
+++ src/pages/userPages/SurveyPage/SurveyPage.tsx
@@ -1,23 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Avatar, Button, Card, Rate, Tag, Timeline, Table, Divider } from 'antd';
+import { Avatar, Button, Card, Rate, Tag, Timeline, Table, Divider, Popconfirm } from 'antd';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 import { IPageData, IPageProps } from '../../../interfaces/page-data';
-import userCover from '../../../assets/content/user-profile.jpg';
-import userAvatar from '../../../assets/content/user-400-1.jpg';
 import API from "../../../utils/API";
-
-const skills = [
-  'HTML',
-  'PHP',
-  'CSS',
-  'SCSS',
-  'ANGUlAR',
-  'REACT',
-  'VUE.JS',
-  'JAVASCRIPT',
-  'TYPESCRIPT'
-];
 
 const PageSurveyPage: React.FC<IPageProps> = props => {
   const { onSetPage } = props;
@@ -33,6 +20,11 @@ const PageSurveyPage: React.FC<IPageProps> = props => {
 
   const onChange = (pagination, filters, sorter) => {
     console.log('params', pagination, filters, sorter);
+  };
+
+  const handleDelete = key => {
+    //API.delete('surveys/'+key);
+    setSurveyData(surveyData.filter(item => item.id !== key));
   };
 
   const pageData: IPageData = {
@@ -70,6 +62,20 @@ const PageSurveyPage: React.FC<IPageProps> = props => {
       dataIndex: 'dateCreated',
       key: 'dateCreated',
       sorter: (a, b) => a.dateCreated - b.dateCreated
+    },
+    {
+      title: 'Action',
+      dataIndex: 'operation',
+      render: (text, record) =>
+        surveyData.length >= 1 ? (<div>
+          <Link to={`/survey-page/${record.id}`}>Edit</Link>
+          {/* <span><a onClick={() => handleEdit(record.id)} href={`#/survey-page/${record.id}`}>Edit</a></span> */}
+          &nbsp;|&nbsp;
+          <Popconfirm title='Sure to delete?' onConfirm={() => handleDelete(record.id)}>
+            <a onClick={e => e.preventDefault()} href='#'>Delete</a>
+          </Popconfirm>
+          </div>
+        ) : null
     }
   ];
 
@@ -78,6 +84,7 @@ const PageSurveyPage: React.FC<IPageProps> = props => {
       <div className='row'>
         <div className='col col-12 col-md-12'>
           <Card title='Survey list' className='component-demo-card'>
+            <Button type='primary' style={{ marginBottom: 16 }}>Add a row</Button>
             <Table pagination={{ pageSize: 5 }} dataSource={surveyData} columns={columns} onChange={onChange}/>
           </Card>
         </div>
